6f5a27a7a8f77b8ebad2cd6657ccc7b465ff59b4
Oleg <pirr31@mail.ru>
2021-02-14 17:38:53 +0300

    Added paths for directory in paths.js

---
+++ config/paths.js
@@ -0,0 +1,9 @@
+const path = require('path')
+
+module.exports = {
+  // Исходники
+  src: path.resolve(__dirname, '../src'),
+
+  // Директория для файлов сборки
+  build: path.resolve(__dirname, '../dist'),
+}
