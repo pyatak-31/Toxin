d5b32da1e8269176cafc49ea01c81cdf57985c03
Oleg <pirr31@mail.ru>
2021-02-17 23:48:14 +0300

    Finished Colors&Types block, started responsive it

--- src/components/ui-logo/ui-logo.js
+++ src/components/ui-logo/ui-logo.js
@@ -1,4 +1,4 @@
-import './logo.scss'
+import './ui-logo.scss'
 import { importAll } from "../../assets/js/functions"
 
 importAll(require.context('./img', false, /.(?:ico|gif|png|jpg|jpeg|webp|svg)$/));
